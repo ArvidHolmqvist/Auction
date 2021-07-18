e11ccb093586a532931c28737451eac1846d9b08
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-07-18 19:52:15 +0200

    init

---
+++ src/test/java/se/ah/auctionservice/AuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ah.auctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
